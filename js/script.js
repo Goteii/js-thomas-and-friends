d437be0a54e28cc19cb4087992133c5ff799c2eb
Goteii <deyssog@gmail.com>
2022-12-08 16:35:50 +0100

    add utils.js, small refactor to reuse code and avoid further HTML code template duplication, change script type to module

--- js/script.js
+++ js/script.js
@@ -1,83 +1,157 @@
+import { MEMBERS, VIDEOS } from "../utils/utils.js";
+
 let menuOpen = false;
 
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    changeBtn();
-  };
+const copyToClipboard = (str) => {
+  const el = document.createElement("textarea");
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  changeBtn();
+};
 
-  const changeBtn = () => {
-      document.getElementById('discord-btn').style.display = 'none';
-      document.getElementById('copied-btn').style.display = "flex";
-      document.getElementById('banner-btn').style.backgroundColor = "rgb(99, 216, 99)";
-      document.getElementById('banner-btn').style.borderColor = "rgb(90, 206, 90)";
-  }
+const changeBtn = () => {
+  document.getElementById("discord-btn").style.display = "none";
+  document.getElementById("copied-btn").style.display = "flex";
+  document.getElementById("banner-btn").style.backgroundColor =
+    "rgb(99, 216, 99)";
+  document.getElementById("banner-btn").style.borderColor = "rgb(90, 206, 90)";
+};
 
-  const moveToSection = (id) => {
-    if(id === 'top') {
-      const scrollOptions = {
-        top: 0,
-        behavior: 'smooth',
-      }
-      window.scrollTo(scrollOptions);
-    }
-    else {
+const moveToSection = (id) => {
+  if (id === "top") {
+    const scrollOptions = {
+      top: 0,
+      behavior: "smooth",
+    };
+    window.scrollTo(scrollOptions);
+  } else {
     const containerIDOffset = document.getElementById(id).offsetTop;
     const scrollOptions = {
       top: containerIDOffset,
-      behavior: 'smooth',
-    }
+      behavior: "smooth",
+    };
     window.scrollTo(scrollOptions);
-    }
   }
+};
 
-  const handleMobileNav = () => {
-    if(!menuOpen) {
-    document.getElementById('slider').style.transform = 'translateX(0%)';
-    document.querySelector('.menu-btn').classList.add('open');
+const handleMobileNav = () => {
+  if (!menuOpen) {
+    document.getElementById("slider").style.transform = "translateX(0%)";
+    document.querySelector(".menu-btn").classList.add("open");
     menuOpen = true;
-    }
-    else {
-      document.getElementById('slider').style.transform = 'translateX(100%)';
-      document.querySelector('.menu-btn').classList.remove('open');
-      menuOpen = false;
-    }
+  } else {
+    document.getElementById("slider").style.transform = "translateX(100%)";
+    document.querySelector(".menu-btn").classList.remove("open");
+    menuOpen = false;
   }
+};
 
-  const wait = (delay = 0) =>
-  new Promise(resolve => setTimeout(resolve, delay));
+const wait = (delay = 0) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
 
-const setVisible = (elementOrSelector, visible) => 
-  (typeof elementOrSelector === 'string'
+const setVisible = (elementOrSelector, visible) =>
+  ((typeof elementOrSelector === "string"
     ? document.querySelector(elementOrSelector)
     : elementOrSelector
-  ).style.display = visible ? 'block' : 'none';
+  ).style.display = visible ? "block" : "none");
 
-setVisible('.content', false);
-setVisible('#loading', true);
+setVisible(".content", false);
+setVisible("#loading", true);
 
-document.addEventListener('DOMContentLoaded', () =>
+document.addEventListener("DOMContentLoaded", () =>
   wait(1200).then(() => {
-    setVisible('.content', true);
-    setVisible('#loading', false);
-  }));
+    setVisible(".content", true);
+    setVisible("#loading", false);
+  })
+);
 
+const showLightbox = (id, title) => {
+  const video = $(`iframe[id='${id}']`);
+  $(".lightbox-container").empty().append(video).fadeIn(1000);
+  $(".lightbox").css("display", "block");
+  $(".lightbox-header").html(title);
+};
 
-  const showLightbox = (id, title) => {
-    const video = $(`iframe[id='${id}']`);
-    $('.lightbox-container').empty().append(video).fadeIn(1000);
-    $('.lightbox').css('display', 'block');
-    $('.lightbox-header').html(title);
-  }
+export const closeLightbox = () => {
+  const videoItem = $(".lightbox").find("iframe");
+  const videoId = $(videoItem).attr("id");
+  const videoContainer = $(`div[id='${videoId}']`);
+  $(videoContainer).append(videoItem);
+  $(".lightbox").css("display", "none");
+};
 
-  const closeLightbox = () => {
-    const videoItem = $('.lightbox').find('iframe');
-    const videoId = $(videoItem).attr('id');
-    const videoContainer = $(`div[id='${videoId}']`);
-    $(videoContainer).append(videoItem);
-    $('.lightbox').css('display', 'none');
-  }
+const getCrewMembers = () => {
+  const wrapper = document.getElementById("crew-wrapper");
+
+  MEMBERS.forEach((el, i) => {
+    const crewWrapper = document.createElement("div");
+    crewWrapper.className = el.parentDivClassName || "crew-member";
+    const crewRank = document.createElement("h2");
+    crewRank.className = "crew-rank";
+    crewRank.innerHTML = el.crewRank;
+    crewWrapper.appendChild(crewRank);
+    const crewImg = document.createElement("img");
+    crewImg.className = "crew-avatar";
+    crewImg.src = el.imgSrc;
+    crewImg.alt = `${el.crewName} discord picture`;
+    crewWrapper.appendChild(crewImg);
+    const crewName = document.createElement("span");
+    crewName.className = "crew-name";
+    crewName.innerHTML = el.crewName;
+    crewWrapper.appendChild(crewName);
+    const crewDescription = document.createElement("span");
+    crewDescription.className = "crew-description";
+    crewDescription.innerHTML = el.crewDescription;
+    crewWrapper.appendChild(crewDescription);
+
+    wrapper.appendChild(crewWrapper);
+  });
+};
+
+const getVideos = () => {
+  const videoWrapper = document.getElementById("video-wrapper");
+
+  VIDEOS.forEach((el) => {
+    const videoDiv = document.createElement("div");
+    videoDiv.className = "video";
+    videoDiv.id = el.id;
+    const videoOverlay = document.createElement("div");
+    videoOverlay.className = "video-overlay";
+    videoOverlay.addEventListener("click", showLightbox(el.id, el.title));
+    videoDiv.appendChild(videoOverlay);
+    const videoDescription = document.createElement("div");
+    videoDescription.class = "video__description";
+    videoDescription.innerHTML = el.title;
+    videoDiv.appendChild(videoDescription);
+    const iframe = document.createElement("iframe");
+    iframe.className = "about__video";
+    iframe.id = el.id;
+    iframe.title = el.title;
+    iframe.width = "100%";
+    iframe.height = "80%";
+    iframe.src = el.iframeSrc;
+    iframe.frameborder = "0";
+    iframe.allowFullscreen = true;
+    videoDiv.appendChild(iframe);
+
+    videoWrapper.appendChild(videoDiv);
+  });
+};
+
+if (
+  document.readyState === "interactive" ||
+  document.readyState === "complete"
+) {
+  getCrewMembers();
+  getVideos();
+  document
+    .getElementById("lightbox")
+    .addEventListener("click", closeLightbox());
+  //global methods
+  window.handleMobileNav = handleMobileNav;
+  window.copyToClipboard = copyToClipboard;
+  window.moveToSection = moveToSection;
+}
---
+++ utils/utils.js
@@ -0,0 +1,174 @@
+export const MEMBERS = [
+  {
+    id: 1,
+    parentDivClassName: "crew-leader",
+    crewRank: "Leader",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "AgatkaNaFecie",
+    crewDescription: `Fucked up junkie trash with burnt out brain. Hes too lazy to do anything in life so he decided to become pro 
+        caller in albion online. He used to main swords and frosts but it turned out too hard to play so he switched to mace. Yes, mace. Fuck mace
+        players. Interests? Good avalonian roads and T8 leathers.`,
+  },
+  {
+    id: 2,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/kolbek-avatar.png",
+    crewName: "Kolbek",
+    crewDescription: "Main roles: Tank and Healer which he's really good at.",
+  },
+  {
+    id: 3,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/maciusg-avatar.png",
+    crewName: "MaciusG",
+    crewDescription:
+      " The most toxic and retarded guy from the whole crew. He has to do a backflip.",
+  },
+  {
+    id: 4,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "ColdOneCherry",
+    crewDescription:
+      " One of the greatest Three Musketers, master of sword and virtuoso of swordsmanship.",
+  },
+  {
+    id: 5,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/goteii-avatar.png",
+    crewName: "Goteii",
+    crewDescription: "Snake.",
+  },
+  {
+    id: 6,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "Zargix",
+    crewDescription:
+      "    The best small-scale healer. Zargix has second shade - becomes very aggressive and selfish while carrying cursed staff.",
+  },
+  {
+    id: 7,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/yourichis-avatar.png",
+    crewName: "Yourichis",
+    crewDescription:
+      "   Bankrupt millionaire from albion who's the only purpose in life is murdering people on roads.",
+  },
+  {
+    id: 8,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/kambudza-avatar.png",
+    crewName: "Kambudza",
+    crewDescription:
+      "       A man which jumped into ZvZ fire having only 2kk of total fame to become a good healer.",
+  },
+  {
+    id: 9,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "Afera24",
+    crewDescription: "        Main role: Axe.",
+  },
+  {
+    id: 10,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "StaryTrzmiel",
+    crewDescription:
+      "     Silent impostor who might fall asleep in the middle of content.",
+  },
+  {
+    id: 11,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "Parkiz",
+    crewDescription:
+      "       One man army - a healer, a dps and a tank at the same time.",
+  },
+  {
+    id: 12,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/leader-avatar.png",
+    crewName: "Murzyn24",
+    crewDescription: "   Spinning axe does WRRRRRRRUUMM.",
+  },
+  {
+    id: 13,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/grim-avatar.png",
+    crewName: "9Gr1m",
+    crewDescription:
+      "     Egyptian pharaoh who got possesed by Anubis and wields only cursed staffs.",
+  },
+  {
+    id: 14,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/mucha-avatar.png",
+    crewName: "vMucha",
+    crewDescription: `     Albionsian prince who can buy out the royal continent, regear all people and still sleep on money. 
+    Plays everything he wants and doesn't go below 1.5k ip.`,
+  },
+  {
+    id: 15,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/berte-avatar.png",
+    crewName: "xBeerte",
+    crewDescription: `    Ice wizard.`,
+  },
+  {
+    id: 16,
+    parentDivClassName: "crew-member",
+    crewRank: "Member",
+    imgSrc: "images/crew-avatars/dziks-avatar.png",
+    crewName: "Dziks",
+    crewDescription: `   Our cryptoboar, who started his journey with investments in albion market and makes big cash off of crypto now.`,
+  },
+];
+
+export const VIDEOS = [
+  {
+    id: "ava_1",
+    title: "Avalonian Roads Pt. 1",
+    iframeSrc: "https://www.youtube.com/embed/G7-LHU4OCpY",
+  },
+  {
+    id: "ava_2",
+    title: "Avalonian Roads Pt. 2",
+    iframeSrc: "https://www.youtube.com/embed/gjS2aMsxP8c",
+  },
+  {
+    id: "ava_3",
+    title: "Avalonian Roads Pt. 3",
+    iframeSrc: "https://www.youtube.com/embed/UNI-ySJkwGI",
+  },
+  {
+    id: "ava_4",
+    title: "Avalonian Roads Pt. 4",
+    iframeSrc: "https://www.youtube.com/embed/tgjCflKKSIA",
+  },
+  {
+    id: "ava_5",
+    title: "Avalonian Roads Pt. 5",
+    iframeSrc: "https://www.youtube.com/embed/a7AIzfEvweQ",
+  },
+  {
+    id: "ava_6",
+    title: "Avalonian Roads Pt. 6",
+    iframeSrc: "https://www.youtube.com/embed/g1MntpSitNU",
+  },
+];
